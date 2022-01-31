4ec167b245109ba0e97a442b4b4808d622e0ccd3
Ab de Kwant <ab@dekwant.com>
2022-01-31 05:55:05 -0500

    Clean-up

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,6 @@ function App() {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <p>Fib Calculator</p>
           <a
             className="App-link"
             href="https://reactjs.org"
