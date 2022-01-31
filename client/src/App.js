41d57c466d651a12f1be5dc61a1288f3733eb64c
Ab de Kwant <ab@dekwant.com>
2022-01-31 05:30:48 -0500

    App change

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ function App() {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>Fib Calculator</p>
           <a
             className="App-link"
             href="https://reactjs.org"
